733babc03abb121666c06823056a9f6514edfbc7
David Aydin <david.aydin6@gmail.com>
2022-05-24 15:37:04 -0400

    Mongo Connected

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+const { urlencoded } = require("express");
 const express = require("express");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
+
+app.use(express.urlencoded({ extended: false }));
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err.message + " is mongo not running?"));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
 app.listen(PORT);
