b62e053c88688b9e6cc3824ba0ea48653a99242c
David Aydin <david.aydin6@gmail.com>
2022-05-24 20:37:12 -0400

    create, new, index, show routes

--- server.js
+++ server.js
@@ -23,11 +23,22 @@ app.post("/products", (req, res) => {
       res.redirect("/products");
     };
 });
-
+// New
+app.get("/products/new", (req, res) => {
+  res.redirect("/products");
+});
 // Index
 app.get("/products", (req, res) => {
   Product.find({}, (error, allProducts) => {
     res.render("index.ejs", { product: allProducts });
+  });
+});
+// Show
+app.get("/products/:id", (req, res) => {
+  Product.findById(req.params.id, (err, foundProduct) => {
+    res.render("show.ejs", {
+      product: foundProduct,
+    });
   });
 });
 app.listen(PORT);
